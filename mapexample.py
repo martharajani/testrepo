6836c041a83866be3023cb24f9587db1fc187569
martharajani <rajanim.itm@gmail.com>
2022-06-16 14:26:24 +0530

    accepted incoming change from enhancement-1

--- mapexample.py
+++ mapexample.py
@@ -1,15 +1,5 @@
 square_num = lambda x: x ** 2
 
-<<<<<<< HEAD
-nums = [1,2,3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-# print("Original List: ",nums)
-def main():
-    result = map(square_num, nums)
-    print("Square the elements of the said list using map():")
-    print(list(result))
-if '__main__' == __name__:
-    main()
-=======
 nums = [1,2,3,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 # print("Original List: ",nums)
 def main():
@@ -18,4 +8,3 @@ def main():
     print(list(result))
 if '__main__' == __name__:
     main()
->>>>>>> feature/enhancement-1
